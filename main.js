4ae48c9aabfb04c655c6bb8370fb0c491094b40c
Richard Gonzales <appsrgonzeb@gmail.com>
2026-04-08 10:19:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 console.log('Ejecutando el contenido de main.js');
 console.log("Variable de ambiente MI_VARIABLE:", process.env.MI_VARIABLE);
-console.log("la variable centralizada es igual a:", env.VARIABLE_CENTRALIZADA);
+console.log("la variable centralizada es igual a:", process.env.VARIABLE_CENTRALIZADA);
